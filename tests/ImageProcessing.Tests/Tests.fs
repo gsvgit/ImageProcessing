e5cae73978ba68e3eb40045cfda6c8e9cc8493bf
gsv <rsdpisuy@gmail.com>
2023-01-11 11:44:32 +0300

    Template initialized.

---
+++ tests/ImageProcessing.Tests/Tests.fs
@@ -0,0 +1,18 @@
+namespace ImageProcessing.Tests
+
+open Expecto
+open ImageProcessing
+
+module SayTests =
+    [<Tests>]
+    let tests =
+        testList "samples" [
+            testCase "Say nothing"
+            <| fun _ ->
+                let subject = Say.nothing ()
+                Expect.equal subject () "Not an absolute unit"
+            testCase "Say hello all"
+            <| fun _ ->
+                let subject = Say.hello "all"
+                Expect.equal subject "Hello all" "You didn't say hello"
+        ]
