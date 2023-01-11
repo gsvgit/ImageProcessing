76d250956cae074e5cfa9633b9b879ca6a9708c8
gsv <rsdpisuy@gmail.com>
2023-01-11 18:15:42 +0300

    Minimal example of filters on GPU

--- src/ImageProcessing/ImageProcessing.fs
+++ src/ImageProcessing/ImageProcessing.fs
@@ -1,5 +1,6 @@
 module ImageProcessing.ImageProcessing
 
+open Brahma.FSharp
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.PixelFormats
 
@@ -64,3 +65,70 @@ let applyFilter (filter: float32[][]) (img: byte[,]) =
         Array.fold2 (fun s x y -> s + x * y) 0.0f filter dataToHandle
 
     Array2D.mapi (fun x y _ -> byte (processPixel x y)) img
+
+
+let applyFilterGPUKernel (clContext: ClContext) localWorkSize =
+
+    let kernel =
+        <@
+            fun (r:Range1D) (img:ClArray<_>) imgW imgH (filter:ClArray<_>) filterD (result:ClArray<_>) ->
+                let p = r.GlobalID0
+                let pw = p % imgW
+                let ph = p / imgW
+
+                let mutable res = 0.0f
+
+                for i in ph - filterD .. ph + filterD do
+                    for j in pw - filterD .. pw + filterD do
+                        let mutable d = 0uy
+                        if i < 0 || i >= imgH || j < 0 || j >= imgW
+                        then d <- img.[p]
+                        else d <- img.[i * imgW + j]
+                        let f = filter.[(i - ph + filterD) * (2 * filterD + 1) + (j - pw + filterD)]
+                        res <- res + (float32 d) * f
+                result.[p] <- byte (int res)
+        @>
+
+    let kernel = clContext.Compile kernel
+
+    fun (commandQueue: MailboxProcessor<_>) (filter: ClArray<float32>) filterD (img: ClArray<byte>) imgH imgW ->
+
+        let result = clContext.CreateClArray(img.Length, allocationMode = AllocationMode.Default)
+
+        let ndRange = Range1D.CreateValid(imgH * imgW, localWorkSize)
+
+        let kernel = kernel.GetKernel()
+        commandQueue.Post(
+            Msg.MsgSetArguments
+                (fun () -> kernel.KernelFunc ndRange img imgW imgH filter filterD result)
+        )
+        commandQueue.Post(Msg.CreateRunMsg<_, _> kernel)
+        result
+
+let applyFiltersGPU (clContext: ClContext) localWorkSize =
+        let kernel = applyFilterGPUKernel clContext localWorkSize
+        let queue = clContext.QueueProvider.CreateQueue()
+        fun (filters: list<float32[][]>) (img: byte[,]) ->
+            let imgH = img.GetLength 0
+            let imgW = img.GetLength 1
+            let img =
+                    [| for x in 0 .. Array2D.length1 img - 1 do
+                       yield! [| for y in 0 .. Array2D.length2 img - 1 -> img.[x, y] |]
+                    |]
+            let clImage = clContext.CreateClArray<_> img
+
+            let  mutable res = Unchecked.defaultof<_>
+
+            for filter in filters do
+                let filter = Array.concat filter
+                let filterD = (Array.length filter) / 2
+                let clFilter = clContext.CreateClArray<_> filter
+                res <- kernel queue clFilter filterD clImage imgH imgW
+                queue.Post(Msg.CreateFreeMsg clFilter)
+
+            let result' = Array.zeroCreate (imgH * imgW)
+            let result' = queue.PostAndReply(fun ch -> Msg.CreateToHostMsg(res, result', ch))
+            let result = Array2D.zeroCreate imgH imgW
+            Array.Parallel.iteri (fun x v -> result.[x / imgW, x % imgW] <-  v) result'
+            clImage.Dispose()
+            result
--- src/ImageProcessing/Main.fs
+++ src/ImageProcessing/Main.fs
@@ -1,13 +1,22 @@
 namespace ImageProcessing
 
+open Brahma.FSharp
+
 module Main =
     let pathToExamples = "/home/gsv/Projects/TestProj2020/src/ImgProcessing/Examples"
     let inputFolder = System.IO.Path.Combine(pathToExamples,"input")
     let demoFile = System.IO.Path.Combine(inputFolder, "armin-djuhic-ohc29QXbS-s-unsplash.jpg")
     [<EntryPoint>]
     let main (argv: string array) =
+        let device = ClDevice.GetFirstAppropriateDevice()
+        printfn $"Device: %A{device.Name}"
+
+        let context = ClContext(device)
+        let applyFiltersGPU = ImageProcessing.applyFiltersGPU context 32
+
         let grayscaleImage = ImageProcessing.loadAs2DArray demoFile
-        let blur = ImageProcessing.applyFilter ImageProcessing.gaussianBlurKernel grayscaleImage
-        let edges = ImageProcessing.applyFilter ImageProcessing.edgesKernel blur
+        //let blur = ImageProcessing.applyFilter ImageProcessing.gaussianBlurKernel grayscaleImage
+        //let edges = ImageProcessing.applyFilter ImageProcessing.edgesKernel blur
+        let edges =  applyFiltersGPU [ImageProcessing.gaussianBlurKernel; ImageProcessing.edgesKernel] grayscaleImage
         ImageProcessing.save2DByteArrayAsImage edges "../../../../../out/demo_grayscale.jpg"
         0
