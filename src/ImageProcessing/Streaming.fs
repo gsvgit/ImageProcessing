b2f2c29011962e0ddb22678479213d460c1f2827
PolinaSavelyeva <lissava00@gmail.com>
2023-02-25 19:17:48 +0300

    Fix Streaming.fs

--- src/ImageProcessing/Streaming.fs
+++ src/ImageProcessing/Streaming.fs
@@ -1,58 +1,58 @@
-module ImageProcessing.Streaming
+module Streaming
 
-open ImageProcessing.ImageProcessing
+open CPUImageProcessing
 
 let listAllFiles dir =
     let files = System.IO.Directory.GetFiles dir
     List.ofArray files
 
 type msg =
-    | Img of Image
+    | Img of MyImage
     | EOS of AsyncReplyChannel<unit>
 
 let imgSaver outDir =
     let outFile (imgName: string) = System.IO.Path.Combine(outDir, imgName)
 
     MailboxProcessor.Start(fun inbox ->
-        let rec loop () = async {
-            let! msg = inbox.Receive()
+        let rec loop () =
+            async {
+                let! msg = inbox.Receive()
 
-            match msg with
-            | EOS ch ->
-                printfn "Image saver is finished!"
-                ch.Reply()
-            | Img img ->
-                printfn $"Save: %A{img.Name}"
-                saveImage img (outFile img.Name)
-                return! loop ()
-        }
+                match msg with
+                | EOS ch ->
+                    printfn "Image saver is finished!"
+                    ch.Reply()
+                | Img img ->
+                    printfn $"Save: %A{img.Name}"
+                    saveMyImageAsImage img (outFile img.Name)
+                    return! loop ()
+            }
 
-        loop ()
-    )
+        loop ())
 
 let imgProcessor filterApplicator (imgSaver: MailboxProcessor<_>) =
 
     let filter = filterApplicator
 
     MailboxProcessor.Start(fun inbox ->
-        let rec loop cnt = async {
-            let! msg = inbox.Receive()
+        let rec loop cnt =
+            async {
+                let! msg = inbox.Receive()
 
-            match msg with
-            | EOS ch ->
-                printfn "Image processor is ready to finish!"
-                imgSaver.PostAndReply EOS
-                printfn "Image processor is finished!"
-                ch.Reply()
-            | Img img ->
-                printfn $"Filter: %A{img.Name}"
-                let filtered = filter img
-                imgSaver.Post(Img filtered)
-                return! loop (not cnt)
-        }
+                match msg with
+                | EOS ch ->
+                    printfn "Image processor is ready to finish!"
+                    imgSaver.PostAndReply EOS
+                    printfn "Image processor is finished!"
+                    ch.Reply()
+                | Img img ->
+                    printfn $"Filter: %A{img.Name}"
+                    let filtered = filter img
+                    imgSaver.Post(Img filtered)
+                    return! loop (not cnt)
+            }
 
-        loop true
-    )
+        loop true)
 
 let processAllFiles inDir outDir filterApplicators =
     let mutable cnt = 0
@@ -61,17 +61,15 @@ let processAllFiles inDir outDir filterApplicators =
         filterApplicators
         |> List.map (fun x ->
             let imgSaver = imgSaver outDir
-            imgProcessor x imgSaver
-        )
+            imgProcessor x imgSaver)
         |> Array.ofList
 
     let filesToProcess = listAllFiles inDir
 
     for file in filesToProcess do
         //while (imgProcessors |> Array.minBy (fun p -> p.CurrentQueueLength)).CurrentQueueLength > 3 do ()
-        (imgProcessors
-         |> Array.minBy (fun p -> p.CurrentQueueLength))
-            .Post(Img(loadAsImage file))
+        (imgProcessors |> Array.minBy (fun p -> p.CurrentQueueLength))
+            .Post(Img(loadAsMyImage file))
 
         cnt <- cnt + 1
 
