3d531a4b2d3e42237a8d078c23db2daa04c0ea40
gsv <rsdpisuy@gmail.com>
2026-05-13 10:18:32 +0300

    Refactored MxM benchmarks.

--- benchmarks/MatrixMultiplication.Benchmarks/Benchmarks.fs
+++ benchmarks/MatrixMultiplication.Benchmarks/Benchmarks.fs
@@ -9,7 +9,8 @@ module DeviceConfig =
     let getDevice () =
         match device with Some d -> d | None -> ClDevice.GetFirstAppropriateDevice()
 
-type K0Benchmark() =
+[<AbstractClass>]
+type MxMBenchmark() =
     let mutable benchmark : (unit -> unit) = Unchecked.defaultof<_>
     let mutable teardown : (unit -> unit) = Unchecked.defaultof<_>
 
@@ -19,16 +20,18 @@ type K0Benchmark() =
     [<Params(8, 16, 32, 64, 128, 256)>]
     member val LWS = 0 with get, set
 
+    abstract member Validate: unit -> unit
+    abstract member CreateKernel: ctx:ClContext * lws:uint -> (MailboxProcessor<Msg> -> ClArray<float32> -> ClArray<float32> -> ClArray<float32> -> int -> unit)
+
     [<GlobalSetup>]
     member this.GlobalSetup() =
-        if this.N % this.LWS <> 0 then
-            failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
+        this.Validate()
         let dev = DeviceConfig.getDevice()
         let ctx = ClContext(dev)
         let q = ctx.QueueProvider.CreateQueue()
         let n = this.N
         let lws = uint this.LWS
-        let kf = multiplyKernel0 ctx lws <@(+)@> <@( * )@> <@0f@>
+        let kernel = this.CreateKernel(ctx, lws)
         let rng = System.Random(42)
         let d1 = Array.init (n * n) (fun _ -> rng.NextSingle())
         let d2 = Array.init (n * n) (fun _ -> rng.NextSingle())
@@ -39,7 +42,7 @@ type K0Benchmark() =
         let sh = Array.zeroCreate<float32>(1)
 
         benchmark <- fun () ->
-            kf q m1 m2 m3 n |> ignore
+            kernel q m1 m2 m3 n |> ignore
             q.PostAndReply(fun ch -> Msg.CreateToHostMsg(sg, sh, ch)) |> ignore
 
         teardown <- fun () ->
@@ -54,201 +57,72 @@ type K0Benchmark() =
 
     [<GlobalCleanup>]
     member this.GlobalCleanup() = teardown()
+
+type K0Benchmark() =
+    inherit MxMBenchmark()
+
+    override this.Validate() =
+        if this.N % this.LWS <> 0 then
+            failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
+
+    override this.CreateKernel(ctx, lws) =
+        let kf = multiplyKernel0 ctx lws <@(+)@> <@( * )@> <@0f@>
+        fun q m1 m2 m3 n -> kf q m1 m2 m3 n |> ignore
 
 type K1Benchmark() =
-    let mutable benchmark : (unit -> unit) = Unchecked.defaultof<_>
-    let mutable teardown : (unit -> unit) = Unchecked.defaultof<_>
+    inherit MxMBenchmark()
 
-    [<Params(256, 512, 1024, 2048)>]
-    member val N = 0 with get, set
-
-    [<Params(8, 16, 32, 64, 128, 256)>]
-    member val LWS = 0 with get, set
-
-    [<GlobalSetup>]
-    member this.GlobalSetup() =
+    override this.Validate() =
         if this.N % this.LWS <> 0 then
             failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
-        let dev = DeviceConfig.getDevice()
-        let ctx = ClContext(dev)
-        let q = ctx.QueueProvider.CreateQueue()
-        let n = this.N
-        let lws = uint this.LWS
+
+    override this.CreateKernel(ctx, lws) =
         let kf = multiplyKernel1 ctx lws <@(+)@> <@( * )@> <@0f@>
-        let rng = System.Random(42)
-        let d1 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let d2 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let m1 = ctx.CreateClArray<float32>(d1, HostAccessMode.NotAccessible)
-        let m2 = ctx.CreateClArray<float32>(d2, HostAccessMode.NotAccessible)
-        let m3 = ctx.CreateClArray<float32>(n * n, HostAccessMode.NotAccessible, deviceAccessMode = DeviceAccessMode.WriteOnly)
-        let sg = ctx.CreateClArray<float32>([|0f|], HostAccessMode.NotAccessible)
-        let sh = Array.zeroCreate<float32>(1)
-
-        benchmark <- fun () ->
-            kf q m1 m2 m3 n |> ignore
-            q.PostAndReply(fun ch -> Msg.CreateToHostMsg(sg, sh, ch)) |> ignore
-
-        teardown <- fun () ->
-            q.Post(Msg.CreateFreeMsg m1)
-            q.Post(Msg.CreateFreeMsg m2)
-            q.Post(Msg.CreateFreeMsg m3)
-            q.Post(Msg.CreateFreeMsg sg)
-            ()
-
-    [<Benchmark>]
-    member this.Run() = benchmark()
-
-    [<GlobalCleanup>]
-    member this.GlobalCleanup() = teardown()
+        fun q m1 m2 m3 n -> kf q m1 m2 m3 n |> ignore
 
 type K2Benchmark() =
-    let mutable benchmark : (unit -> unit) = Unchecked.defaultof<_>
-    let mutable teardown : (unit -> unit) = Unchecked.defaultof<_>
+    inherit MxMBenchmark()
 
-    [<Params(256, 512, 1024, 2048)>]
-    member val N = 0 with get, set
-
-    [<Params(8, 16, 32, 64, 128, 256)>]
-    member val LWS = 0 with get, set
-
-    [<GlobalSetup>]
-    member this.GlobalSetup() =
+    override this.Validate() =
         if this.N % this.LWS <> 0 then
             failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
-        let dev = DeviceConfig.getDevice()
-        let ctx = ClContext(dev)
-        let q = ctx.QueueProvider.CreateQueue()
-        let n = this.N
-        let lws = uint this.LWS
+
+    override this.CreateKernel(ctx, lws) =
         let kf = multiplyKernel2 ctx lws <@(+)@> <@( * )@> <@0f@>
-        let rng = System.Random(42)
-        let d1 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let d2 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let m1 = ctx.CreateClArray<float32>(d1, HostAccessMode.NotAccessible)
-        let m2 = ctx.CreateClArray<float32>(d2, HostAccessMode.NotAccessible)
-        let m3 = ctx.CreateClArray<float32>(n * n, HostAccessMode.NotAccessible, deviceAccessMode = DeviceAccessMode.WriteOnly)
-        let sg = ctx.CreateClArray<float32>([|0f|], HostAccessMode.NotAccessible)
-        let sh = Array.zeroCreate<float32>(1)
-
-        benchmark <- fun () ->
-            kf q m1 m2 m3 n |> ignore
-            q.PostAndReply(fun ch -> Msg.CreateToHostMsg(sg, sh, ch)) |> ignore
-
-        teardown <- fun () ->
-            q.Post(Msg.CreateFreeMsg m1)
-            q.Post(Msg.CreateFreeMsg m2)
-            q.Post(Msg.CreateFreeMsg m3)
-            q.Post(Msg.CreateFreeMsg sg)
-            ()
-
-    [<Benchmark>]
-    member this.Run() = benchmark()
-
-    [<GlobalCleanup>]
-    member this.GlobalCleanup() = teardown()
+        fun q m1 m2 m3 n -> kf q m1 m2 m3 n |> ignore
 
 type K3Benchmark() =
-    let mutable benchmark : (unit -> unit) = Unchecked.defaultof<_>
-    let mutable teardown : (unit -> unit) = Unchecked.defaultof<_>
-
-    [<Params(256, 512, 1024, 2048)>]
-    member val N = 0 with get, set
-
-    [<Params(8, 16, 32, 64, 128, 256)>]
-    member val LWS = 0 with get, set
+    inherit MxMBenchmark()
 
     [<Params(1, 2, 4, 8)>]
     member val WPT = 0 with get, set
 
-    [<GlobalSetup>]
-    member this.GlobalSetup() =
+    override this.Validate() =
         if this.N % this.LWS <> 0 then
             failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
         if this.N % this.WPT <> 0 then
             failwithf "N=%d not divisible by WPT=%d" this.N this.WPT
         if this.LWS % this.WPT <> 0 then
             failwithf "LWS=%d not divisible by WPT=%d" this.LWS this.WPT
-        let dev = DeviceConfig.getDevice()
-        let ctx = ClContext(dev)
-        let q = ctx.QueueProvider.CreateQueue()
-        let n = this.N
-        let lws = uint this.LWS
-        let wpt = uint this.WPT
-        let kf = multiplyKernel3 ctx lws wpt <@(+)@> <@( * )@> <@0f@>
-        let rng = System.Random(42)
-        let d1 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let d2 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let m1 = ctx.CreateClArray<float32>(d1, HostAccessMode.NotAccessible)
-        let m2 = ctx.CreateClArray<float32>(d2, HostAccessMode.NotAccessible)
-        let m3 = ctx.CreateClArray<float32>(n * n, HostAccessMode.NotAccessible, deviceAccessMode = DeviceAccessMode.WriteOnly)
-        let sg = ctx.CreateClArray<float32>([|0f|], HostAccessMode.NotAccessible)
-        let sh = Array.zeroCreate<float32>(1)
 
-        benchmark <- fun () ->
-            kf q m1 m2 m3 n |> ignore
-            q.PostAndReply(fun ch -> Msg.CreateToHostMsg(sg, sh, ch)) |> ignore
-
-        teardown <- fun () ->
-            q.Post(Msg.CreateFreeMsg m1)
-            q.Post(Msg.CreateFreeMsg m2)
-            q.Post(Msg.CreateFreeMsg m3)
-            q.Post(Msg.CreateFreeMsg sg)
-            ()
-
-    [<Benchmark>]
-    member this.Run() = benchmark()
-
-    [<GlobalCleanup>]
-    member this.GlobalCleanup() = teardown()
+    override this.CreateKernel(ctx, lws) =
+        let kf = multiplyKernel3 ctx lws (uint this.WPT) <@(+)@> <@( * )@> <@0f@>
+        fun q m1 m2 m3 n -> kf q m1 m2 m3 n |> ignore
 
 type K4Benchmark() =
-    let mutable benchmark : (unit -> unit) = Unchecked.defaultof<_>
-    let mutable teardown : (unit -> unit) = Unchecked.defaultof<_>
-
-    [<Params(256, 512, 1024, 2048)>]
-    member val N = 0 with get, set
-
-    [<Params(8, 16, 32, 64, 128, 256)>]
-    member val LWS = 0 with get, set
+    inherit MxMBenchmark()
 
     [<Params(1, 2, 4, 8)>]
     member val TTS = 0 with get, set
 
-    [<GlobalSetup>]
-    member this.GlobalSetup() =
+    override this.Validate() =
+        if this.N % this.LWS <> 0 then
+            failwithf "N=%d not divisible by LWS=%d" this.N this.LWS
         if this.N % this.TTS <> 0 then
             failwithf "N=%d not divisible by TTS=%d" this.N this.TTS
         if this.LWS % this.TTS <> 0 then
             failwithf "LWS=%d not divisible by TTS=%d" this.LWS this.TTS
-        let dev = DeviceConfig.getDevice()
-        let ctx = ClContext(dev)
-        let q = ctx.QueueProvider.CreateQueue()
-        let n = this.N
-        let lws = uint this.LWS
-        let tts = uint this.TTS
-        let kf = multiplyKernel4 ctx lws tts <@(+)@> <@( * )@> <@0f@>
-        let rng = System.Random(42)
-        let d1 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let d2 = Array.init (n * n) (fun _ -> rng.NextSingle())
-        let m1 = ctx.CreateClArray<float32>(d1, HostAccessMode.NotAccessible)
-        let m2 = ctx.CreateClArray<float32>(d2, HostAccessMode.NotAccessible)
-        let m3 = ctx.CreateClArray<float32>(n * n, HostAccessMode.NotAccessible, deviceAccessMode = DeviceAccessMode.WriteOnly)
-        let sg = ctx.CreateClArray<float32>([|0f|], HostAccessMode.NotAccessible)
-        let sh = Array.zeroCreate<float32>(1)
 
-        benchmark <- fun () ->
-            kf q m1 m2 m3 n |> ignore
-            q.PostAndReply(fun ch -> Msg.CreateToHostMsg(sg, sh, ch)) |> ignore
-
-        teardown <- fun () ->
-            q.Post(Msg.CreateFreeMsg m1)
-            q.Post(Msg.CreateFreeMsg m2)
-            q.Post(Msg.CreateFreeMsg m3)
-            q.Post(Msg.CreateFreeMsg sg)
-            ()
-
-    [<Benchmark>]
-    member this.Run() = benchmark()
-
-    [<GlobalCleanup>]
-    member this.GlobalCleanup() = teardown()
+    override this.CreateKernel(ctx, lws) =
+        let kf = multiplyKernel4 ctx lws (uint this.TTS) <@(+)@> <@( * )@> <@0f@>
+        fun q m1 m2 m3 n -> kf q m1 m2 m3 n |> ignore
